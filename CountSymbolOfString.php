b0e11f4eb6225fe2b91ceef91a276ca759524b67
Alexander <pchp250g8@tutamail.com>
2026-04-23 12:08:18 +0300

    Created Project CountSymbolOfString

---
+++ CountSymbolOfString.php
@@ -0,0 +1,23 @@
+<?php
+    echo("Input a string:\r\n");
+    $strLine = chop(fgets(STDIN));
+    echo("Input a symbol\r\n");
+    $chSym = chop(fgetc(STDIN));
+    $nLen = strlen($strLine);
+    $c = 0;
+    for ($i = 0; $i < $nLen; $i++)
+    {
+        if ($chSym == $strLine[$i])
+        {
+            $c++;
+        }  
+    }
+    $fltPart = ($c / $nLen) * 100;
+    printf("The symbol \"%s\" occurs %d times\r\n", $chSym, $c);
+    printf
+    (
+        "The symbol \"%s\" occupies %.2f %% of the string \"%s\"\r\n", 
+        $chSym, $fltPart, $strLine
+    );
+    fgetc(STDIN); 
+?>
